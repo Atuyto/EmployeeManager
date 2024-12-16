5dcf1b24feb3d59d65b5ad732b11f720b8936500
alexa <alexandresoune@gmail..com>
2024-12-16 20:33:20 +0100

    init

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,12 +30,11 @@ tasks.named<JacocoReport>("jacocoTestReport") {
 
 sonarqube  {
     properties {
-        property("sonar.projectKey", "Atuyto-Fac-groupe_The-Rift-Mobile_53065491-1184-4c29-90c8-48ee261aa76f")
-        property("sonar.projectName", "The-Rift-Mobile")
-        property("sonar.organization", "Atuyto-Fac-groupe")
+        property("sonar.projectKey", "EmployeeManager")
+        property("sonar.projectName", "The-Rift-EmployeeManager")
         property("sonar.sources", "src/main/java/main")
         property("sonar.host.url", "https://condor-funky-completely.ngrok-free.app")
-        property("sonar.login", "squ_69f986dd2782786dec9596bfd09b1de4c9dd6a76")
+        property("sonar.token", "squ_69f986dd2782786dec9596bfd09b1de4c9dd6a76")
         property("sonar.java.binaries", file("app/build/classes/java/debug"))
         property("sonar.jacoco.reportPaths", "app/reports/jacoco/test/testExec.xml")
     }
